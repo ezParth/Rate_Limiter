aa9d0e1fb68a98a02f7dfddc93abc1b68e90c87f
ezParth <parthsingh55m@gmail.com>
2025-08-21 12:06:47 +0530

    Added waitgroup

--- temp/concurrency/main.go
+++ temp/concurrency/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -11,12 +12,53 @@ type Order struct {
 	Status string
 }
 
+var status = []string{"Pending", "Shipped", "Delivered"}
+
 func main() {
+	var wg sync.WaitGroup
+
 	orders := createOrder(20)
 
-	processOrders(orders)
+	wg.Add(3)
+
+	go func() {
+		defer wg.Done()
+		processOrders(orders)
+	}()
+
+	go func() {
+		defer wg.Done()
+		updateOrders(orders)
+	}()
+
+	go func() {
+		defer wg.Done()
+		reportOrderStatus(orders)
+	}()
 
 	fmt.Println("All orders completed, Exiting!")
+}
+
+func updateOrders(orders []*Order) {
+	for _, order := range orders {
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+
+		order.Status = status[rand.Intn(3)]
+		fmt.Printf("Updated Orders %d, status %s \n", order.ID, order.Status)
+	}
+}
+
+func reportOrderStatus(orders []*Order) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(1000 * time.Millisecond)
+		fmt.Println("\n --- Orders Status Report --- ")
+
+		for _, order := range orders {
+			fmt.Printf("Order %d: %s\n", order.ID, order.Status)
+		}
+	}
+
+	fmt.Print("-------------------------------\n")
 }
 
 func processOrders(orders []*Order) {
